ee09fce9f932c541dfa6f6877db706d43592aa06
a-u19 <uarjun@hotmail.co.uk>
2024-12-05 14:46:46 +0000

    Finished day 4 part 2

--- day_4.py
+++ day_4.py
@@ -1,4 +1,5 @@
 import time
+import regex as re
 
 def part_1(inp_filepath: str) -> int:
     # Read input file into a 2D list
@@ -45,7 +46,28 @@ def match_word(data: list[str], word: str, x: int, y: int, dx: int, dy: int) -> 
     return True
 
 
+def part_2(inp_filepath:str) -> int:
+    data = [line.strip() for line in open(inp_filepath).readlines()]
+    total_matches = 0
+    patterns = [tuple((r'M.S', r'M.S')), tuple((r'S.S', r'M.M')), tuple((r'S.M', r'S.M')), tuple((r'M.M', r'S.S'))]
+    for pattern in patterns:
+        for line_idx, line in enumerate(data):
+            # print(pattern, line_idx, line)
+            try:
+                matches = re.finditer(pattern[0], line, overlapped=True)
+                for match in matches:
+                    # print(line_idx, match.span())
+                    if re.match(r".A.", data[line_idx+1][match.start():match.end()]) and re.match(pattern[1], data[line_idx+2][match.start():match.end()]):
+                        total_matches += 1
+                        # print(line_idx, match.span(), pattern)
+            except IndexError:
+                pass
+        # data = [''.join(reversed(col)) for col in zip(*data)]
+
+    return total_matches
+
 # Run the program
 start_time = time.time_ns()
 print(f"The answer to part one is {part_1('day_4_input.txt')}")
+print(f"The answer to part two is {part_2('day_4_input.txt')}")
 print(f"Time taken: {time.time_ns() - start_time} nanoseconds = {(time.time_ns() - start_time)/1e+9} seconds")
